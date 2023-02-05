6327868a5d927eb138c1afbee1c1a28143660f87
Mikhael  Danilov <mikhael.danilov@gmail.com>
2023-02-06 01:08:40 +0300

    more fixes

--- scripts/actors/Spawner.lua
+++ scripts/actors/Spawner.lua
@@ -21,65 +21,64 @@ local now = 0
 
 local rat = RPD.MobFactory:mobByName("Rat")
 
-local function getCell()
-local level = RPD.Dungeon.level
-local cell = math.random(1,level:getLength()-1)
---[[if level.solid[cell] or level.nearWalls[cell] or level.pit[cell] or level.fieldOfView[cell] or (not level.passable[cell]) or level.map[cell] == RPD.Terrain.WALL or level.map[cell] == RPD.Terrain.WALL_DECO or RPD.Actor:findChar(cell) or cell == 0 or level.map[cell] == RPD.Terrain.BOOKSHELF or level.map[cell] == RPD.Terrain.CHASM or level.map[cell] == RPD.Terrain.BARRICADE or level:getTopLevelObject(cell) or level.map[cell] == 48 or level.map[cell] == 63 then
---]]
-if not rat:canSpawnAt(level, cell) then
-return getCell()
-end
-return cell
-end
 
 return actor.init({
-getCell = function()
-return getCell()
-end,
-activate = function()
-local level = RPD.Dungeon.level
-if not storage.get("level_spawner") then
-if level:getLength() > 64*64 then
-max = math.floor(level:getLength()/200)
-else
-max = math.floor(level:getLength()/100)
-end
-for i = 1,max  do
-local mob = Best:mob(level)
-mob:setPos(getCell())
-level:spawnMob(mob)
-end
-storage.put("level_spawner",true)
-end
-end,
-act = function()
-local level = RPD.Dungeon.level
-if level:getLength() > 64*64 then
-max = math.floor(level:getLength()/200)
-else
-max = math.floor(level:getLength()/100)
-end
-now = 0
-local level = RPD.Dungeon.level
-for i = 0, level:getLength()-1 do
-if RPD.Actor:findChar(i) then
-now = now + 1
-end
-end
-if now > max then
-now = 0
-return true
-end
-local mob = Best:mob(level)
-mob:setPos(getCell())
-level:spawnMob(mob)
-RPD.setAi(mob,"Wandering")
-return true
-end,
-actionTime = function()
-if RPD.Dungeon.level:getLength() > 64*64 then
-return 60
-end
-return 30
-end
+    getCell = function()
+        return RPD.getCell()
+    end,
+    activate = function()
+        local level = RPD.Dungeon.level
+        if not storage.get("level_spawner") then
+            if level:getLength() > 64 * 64 then
+                max = math.floor(level:getLength() / 200)
+            else
+                max = math.floor(level:getLength() / 100)
+            end
+            for i = 1, max do
+                local mob = Best:mob(level)
+                local cell = RPD.getCell()
+                if cell > 0 then
+                    mob:setPos(cell)
+                    level:spawnMob(mob)
+                else
+                    break
+                end
+            end
+            storage.put("level_spawner", true)
+        end
+    end,
+    act = function()
+        local level = RPD.Dungeon.level
+        local len = level:getLength()
+        if len > 64 * 64 then
+            max = math.floor(len / 200)
+        else
+            max = math.floor(len / 100)
+        end
+        now = 0
+
+        for i = 0, len - 1 do
+            if RPD.Actor:findChar(i) then
+                now = now + 1
+            end
+        end
+        if now > max then
+            now = 0
+            return true
+        end
+        local mob = Best:mob(level)
+        local cell = RPD.getCell()
+        if cell > 0 then
+            mob:setPos(cell)
+            level:spawnMob(mob)
+            RPD.setAi(mob, "Wandering")
+        end
+        return true
+    end,
+    actionTime = function()
+        if RPD.Dungeon.level:getLength() > 64 * 64 then
+            return 60
+        end
+        return 30
+    end
 })
